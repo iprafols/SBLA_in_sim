76849dc7ef9419b2c745dfb6141eaa6ae7896949
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2025-10-28 14:38:14 +0100

    fixed naming bug

--- sbla_in_sim/line_fit_utils.py
+++ sbla_in_sim/line_fit_utils.py
@@ -228,7 +228,7 @@ def fit_lines(rays_base_name, input_extension, noise, start_z, start_b,
 
             all_species_lines[f"{species} N [cm^-2]"] = minimizer.values["column_density"]
             all_species_lines[f"{species} b [km/s]"] = minimizer.values["impact_parameter"]
-            all_species_lines[f"{species} f"] = minimizer.values["redshift"]
+            all_species_lines[f"{species} zfit"] = minimizer.values["redshift"]
 
             flux_fit = leasts_squares.compute_line_profile(
                 minimizer.values["column_density"],
